14f2035e6ae1796d291683bd84eaf0ca9744759b
Peter Stojcsics <stoto@stoto.net>
2018-02-01 12:07:55 +0100

    module: mesh bed leveling
    Integrate mesh bed leveling with setting up interpolator as a perfectly flat bed. The printer should behave normally with leveling turned on, but currently using 0 compensation at every coordinate.
    
    Signed-off-by: Peter Stojcsics <stoto@stoto.net>

--- klippy/extras/mesh_bed_leveling.py
+++ klippy/extras/mesh_bed_leveling.py
@@ -3,7 +3,33 @@ from scipy.interpolate import griddata
 
 
 class MeshBedLeveling:
+    def __init__(self, config):
+        self.printer = config.get_printer()
+        self.bed_max_x = config.getFloat('bed_max_x', 100.)
+        self.bed_max_y = config.getFloat('bed_max_y', 100.)
+        self.interpolator = Interpolator(self.bed_max_x, self.bed_max_y)
+        self.interpolator.setup_interpolation([[0, 0], [self.bed_max_x, 0], [0, self.bed_max_y],
+                                               [self.bed_max_x, self.bed_max_y]], [0, 0, 0, 0])
 
+        self.toolhead = None
+        gcode = self.printer.lookup_object('gcode')
+        gcode.set_move_transform(self)
+
+    def printer_state(self, state):
+        if state == 'connect':
+            self.toolhead = self.printer.lookup_object('toolhead')
+
+    def get_position(self):
+        x, y, z, e = self.toolhead.get_position()
+        return [x, y, z - self.interpolator.compensation_for_coordinate(x, y), e]
+
+    def move(self, newpos, speed):
+        x, y, z, e = newpos
+        self.toolhead.move([x, y, z + self.interpolator.compensation_for_coordinate(x, y), e],
+                           speed)
+
+
+class Interpolator:
     def __init__(self, bed_max_x, bed_max_y):
         self.bed_max_x = bed_max_x
         self.bed_max_y = bed_max_y
@@ -20,3 +46,9 @@ class MeshBedLeveling:
 
     def is_inside_bed(self, x, y):
         return x <= self.bed_max_x and y <= self.bed_max_y
+
+
+def load_config(config):
+    if config.get_name() != 'mesh_bed_leveling':
+        raise config.error("Invalid mesh_bed_leveling config name")
+    return MeshBedLeveling(config)
